f6aa6cbf49f0bc4b12dadc84dc8a6b35de3432fe
Caden-O <caden.ortt@edu.sait.ca>
2026-03-09 11:23:55 -0600

    Reset Project

---
+++ app/(tabs)/EmployeeForm.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EmployeeForm = () => {
+  return (
+    <View>
+      <Text>employeeForm</Text>
+    </View>
+  )
+}
+
+export default EmployeeForm
+
+const styles = StyleSheet.create({})
