f7fc9c6769c5a344296afa8e661b8fcc47164f51
Mariel Bananola <theplanetm4rs@gmail.com>
2026-03-10 20:59:06 -0600

    Landing Page Create
    edited and styled the index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,59 @@
+import { useRouter } from "expo-router";
 import React from "react";
-import { View } from "react-native";
-import SignInForm from "./(tabs)/SignInForm";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function Index() {
+export default function App() {
+  const router = useRouter();
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <SignInForm />
+    <View style={styles.container}>
+      <Text style={styles.title}>Employee Forms App!</Text>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => router.push("/(tabs)/EmployeeForm")}
+      >
+        <Text style={styles.buttonText}>Employee Form</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { marginTop: 20 }]}
+        onPress={() => router.push("/(tabs)/SignInForm")}
+      >
+        <Text style={styles.buttonText}>Sign In Form</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, { marginTop: 20 }]}
+        onPress={() => router.push("/(tabs)/SignUpForm")}
+      >
+        <Text style={styles.buttonText}>Sign Up Form</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#007AFF",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
